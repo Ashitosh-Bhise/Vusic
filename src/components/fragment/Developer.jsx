344b9b26cc60b29fb4c12ad57ffac8ce4a8cd46d
Ashitosh Bhise <132629679+Ashitosh-Bhise@users.noreply.github.com>
2024-02-22 13:26:55 +0530

    Update Developer.jsx

--- src/components/fragment/Developer.jsx
+++ src/components/fragment/Developer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../assets/scss/Developer.scss';
 import {IconButton} from "@material-ui/core";
 import AvatarImage from "../assets/img/avatar.jpg";
-import {Facebook, Instagram, LinkedIn, Portrait, Twitter} from "@material-ui/icons";
+import {LinkedIn, Portrait, Twitter} from "@material-ui/icons";
 
 const Developer = () => {
     return (
@@ -10,34 +10,30 @@ const Developer = () => {
             <h3 className={"Developer-head"}>Meet the developer</h3>
             <div className="Developer-profile">
                 <div className="Developer-profileCard">
-                    <img src={AvatarImage} alt="Profile"/>
+                   
                     <div className={"Card-details"}>
-                        <h3>Vishal Singh</h3>
+                        <h3>Ashitosh Bhise</h3>
                         <p>Full Stack developer</p>
                         <p>Competitive Coder</p>
                     </div>
                 </div>
                 <div className="Developer-profileDetails">
-                    <p>A Computer Science and Engineering Student at Lovely Professional University, Punjab.</p>
-                    <p>Graduating in 2022 and looking for a responsible position to gain practical knowledge</p>
+                    
+                    <p>Complete Fullstack web development bootcamp in 2022 and looking for a responsible position to gain practical knowledge</p>
                     <p>A full-stack web developer and a Competitive coder.</p>
                     <p>I love designing fully responsive websites.</p>
                     <p>I have a keen interest in developing projects, whenever I want to learn something new.</p>
-                    <p>And a blogger.</p>
+                    
                     <div className="Card-btn">
-                        <IconButton target={"_blank"}  href={"https://www.facebook.com/vishal.kld"} title={"vishal.kld"}>
-                            <Facebook/>
-                        </IconButton>
-                        <IconButton target={"_blank"} href={"https://twitter.com/Vishal_kld"}  title={"Vishal_kld"}>
+                        
+                      
+                        <IconButton target={"_blank"} href={"https://twitter.com/ashitosh_bhise"}  title={"ashitosh_bhise"}>
                             <Twitter/>
                         </IconButton>
-                        <IconButton target={"_blank"} href={"https://www.linkedin.com/in/vishalsingh-/"}  title={"vishalsingh-"}>
+                        <IconButton target={"_blank"} href={"https://www.linkedin.com/in/ashitosh-bhise-4388221b1/"}  title={"ashitosh-bhise"}>
                             <LinkedIn/>
                         </IconButton>
-                        <IconButton target={"_blank"} href={"https://www.instagram.com/vishalsingh023/"}  title={"vishalsingh023"}>
-                            <Instagram/>
-                        </IconButton>
-                        <IconButton target={"_blank"} href={"https://vishal.thetechnician.in/"}  title={"Web Portfolio"}>
+                        <IconButton target={"_blank"} href={"https://ashitosh-bhise-io.vercel.app/"}  title={"Web Portfolio"}>
                             <Portrait/>
                         </IconButton>
                     </div>
